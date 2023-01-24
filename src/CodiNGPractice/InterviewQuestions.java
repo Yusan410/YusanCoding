b878b51c07a3bcccb53a102b65864bb354fae52d
Yusan410 <yissan517@gmail.com>
2023-01-23 20:53:40 -0600

    Yusan Coding

---
+++ src/CodiNGPractice/InterviewQuestions.java
@@ -0,0 +1,21 @@
+package CodiNGPractice;
+
+public class InterviewQuestions {
+	
+	public static void main(String[] args) {
+		
+		
+		String a ="Almira";
+		
+		String b = "";
+		
+		for(int i =a.length()-1;i>=0;i--) {
+			
+			b += a.charAt(i);
+			
+			
+		}
+		System.out.println(b);
+	}
+
+}
